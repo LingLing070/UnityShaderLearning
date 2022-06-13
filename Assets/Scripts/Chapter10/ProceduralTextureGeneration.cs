f9663ed80ecd226c626c62d28e21599951b30ed8
LingLing70 <liqinling666@gmail.com>
2022-06-13 17:32:33 +0800

    2022/6/13-10.3

---
+++ Assets/Scripts/Chapter10/ProceduralTextureGeneration.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ProceduralTextureGeneration : MonoBehaviour
+{
+    public Material material = null;
+
+	#region Material properties
+	[SerializeField, SetProperty("textureWidth")]
+	private int m_textureWidth = 512;
+	public int textureWidth
+	{
+		get
+		{
+			return m_textureWidth;
+		}
+		set
+		{
+			m_textureWidth = value;
+			_UpdateMaterial();
+		}
+	}
+
+	[SerializeField, SetProperty("backgroundColor")]
+	private Color m_backgroundColor = Color.white;
+	public Color backgroundColor
+	{
+		get
+		{
+			return m_backgroundColor;
+		}
+		set
+		{
+			m_backgroundColor = value;
+			_UpdateMaterial();
+		}
+	}
+
+	[SerializeField, SetProperty("circleColor")]
+	private Color m_circleColor = Color.yellow;
+	public Color circleColor
+	{
+		get
+		{
+			return m_circleColor;
+		}
+		set
+		{
+			m_circleColor = value;
+			_UpdateMaterial();
+		}
+	}
+
+	[SerializeField, SetProperty("blurFactor")]
+	private float m_blurFactor = 2.0f;
+	public float blurFactor
+	{
+		get
+		{
+			return m_blurFactor;
+		}
+		set
+		{
+			m_blurFactor = value;
+			_UpdateMaterial();
+		}
+	}
+	#endregion
+
+
+}
